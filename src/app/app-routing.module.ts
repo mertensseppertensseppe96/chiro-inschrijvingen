8f8e4bfb57b0f77085a5f94d7b76329c28bfd7fb
mertensseppertensseppe96 <mertensseppe@icloud.com>
2019-08-19 16:11:30 +0200

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import { LidComponent } from './lid/lid.component';
+import { AboutComponent } from './components/about/about.component';
+const routes: Routes = [
+  { path: '' ,component: LidComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatMenuModule,MatButtonModule, MatIconModule, MatSidenav, MatSidenavModule } from '@angular/material'
+
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -10,11 +12,16 @@ import { AppComponent } from './app.component';
 import { LidComponent } from './lid/lid.component';
 import { LidLijstComponent } from './lid-lijst/lid-lijst.component';
 import { environment } from '../environments/environment';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     LidComponent,
-    LidLijstComponent
+    LidLijstComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +29,12 @@ import { environment } from '../environments/environment';
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
-    FormsModule
+    FormsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
